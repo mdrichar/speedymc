25ab4a3147eb3f44d026156ff878509f3689b8d4
Mark Richards <markedup@gmail.com>
2017-04-13 22:11:54 -0400

    Add flash app starter

---
+++ flash/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+import json
+
+def index(request):
+    return HttpResponse("Trying to run this here and there.")
+
+# Create your views here.
--- speedymc/settings.py
+++ speedymc/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'flash.apps.FlashConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
