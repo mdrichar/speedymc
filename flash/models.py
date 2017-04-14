59289ed343e64181c71e4cf7e54bb944d7722c4e
Mark Richards <markedup@gmail.com>
2017-04-14 19:48:13 -0400

    Add db models and listing view

--- flash/models.py
+++ flash/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+
+class Factoid(models.Model):
+    fid = models.IntegerField(default=0)
+    factor1 = models.IntegerField(default=0)
+    factor2 = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.factor1) + "*" + str(self.factor2) + "=" + str(self.factor1*self.factor2)
+
+class BinaryFact(models.Model):
+    operand1 = models.IntegerField(default=0)
+    operand2 = models.IntegerField(default=0)
+    operator = models.CharField(max_length=1)
+
+    def __str__(self):
+        return str(self.operand1) + self.operator + str(self.operand2)
+
+class User(models.Model):
+    name = models.CharField(max_length=40);
+
+    def __str__(self):
+        return str(self.name) 
+
+class QuestionSet(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=512)
+    binary_facts = models.ManyToManyField(BinaryFact, related_name='qsets')
+
+    def __str__(self):
+        return str(self.name) 
+
+class Bout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_set = models.ForeignKey(QuestionSet, on_delete=models.CASCADE)
+    elapsed = models.IntegerField(default=0)
+    correct_cnt = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id) + str(self.user) + str(self.question_set) + ":" + str(self.elapsed)
+
+
+class ResponseTiming(models.Model):
+    bout = models.ForeignKey(Bout, on_delete=models.CASCADE)
+    binary_fact = models.ForeignKey(BinaryFact, on_delete=models.CASCADE)
+    elapsed = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return str(self.bout) + str(self.binary_fact) + ":" + str(self.elapsed)
+
+
+
+
+
--- flash/views.py
+++ flash/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from .models import QuestionSet, BinaryFact, ResponseTiming, Bout, User
 import json
 
 def index(request):
     return HttpResponse("Trying to run this here and there, plus live modifications.")
 
+def listing(request):
+    #factoid_listing = Factoid.objects.all()
+    factoid_listing = BinaryFact.objects.all()
+    template = loader.get_template('flash/listing.html')
+    context = {
+        'factoid_listing' : factoid_listing, 
+    }
+    return HttpResponse(template.render(context,request))
 # Create your views here.
